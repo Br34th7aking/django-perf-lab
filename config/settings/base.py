e72ee6e9dc8a347c7f4fc0dccb95d06b3b7d7aa2
Abhijit Raj <19046817+Br34th7aking@users.noreply.github.com>
2026-09-07 13:27:55 +0530

    feat: settings package with postgres wiring

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_w4mo0##$y-qj-&7v5gh3tw68c8+d_jgkia#qug217lz*lqgc@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("POSTGRES_DB", "perflab"),
+        'USER': os.environ.get("POSTGRES_USER", "perflab"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", "perflab"),
+        'HOST': os.environ.get("POSTGRES_HOST", "localhost"),
+        'PORT': '5432',
     }
 }
 
